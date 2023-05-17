e7a6a8818390bf06ef42bcdca368e9b255554a81
Dhvanil-CSE <130285680+Dhvanil-CSE@users.noreply.github.com>
2023-05-18 00:06:51 +0530

    Update num_theory.py

--- assignment-2/num_theory.py
+++ assignment-2/num_theory.py
@@ -1,15 +1,7 @@
 from sage.all import *
 import random
 k=int(input())
-plist=[]
-for i in range(10**(k-1),10**k):
-	c=1
-	for j in range(2,i//2+1):
-		if i%j==0:
-			c=0
-			break
-	if c==1:
-		plist.append(i)
+plist=prime_range(10**k-1,10**k)
 p1=random.choice(plist)
 plist.remove(p1)
 p2=random.choice(plist)
