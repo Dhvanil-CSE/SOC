ee1e48c113df73eebb3fc7d5e85a372d02f65d64
Dhvanil-CSE <130285680+Dhvanil-CSE@users.noreply.github.com>
2023-05-18 00:52:05 +0530

    Update num_theory.py

--- assignment-2/num_theory.py
+++ assignment-2/num_theory.py
@@ -1,10 +1,8 @@
 from sage.all import *
 import random
 k=int(input())
-plist=prime_range(10**k-1,10**k)
-p1=random.choice(plist)
-plist.remove(p1)
-p2=random.choice(plist)
+p1=random_prime(10**k-1,10**k)
+p2=random_prime(10**k-1,10**k)
 
 a=inverse_mod(p1,p2)
 b=inverse_mod(p2,p1)-p1
